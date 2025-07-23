5fbc8074058895698483ec1afaf0da81ad498d34
GGN_2015 <premierbob@qq.com>
2025-07-23 19:35:43 +0800

    v2025-07-23

---
+++ src/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 使用 readlink -f 获取脚本的绝对路径（兼容符号链接）
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+# 切换到脚本所在目录
+cd "$SCRIPT_DIR" || exit
+
+# 优化编译
+g++ -O3 -march=native -flto -fno-semantic-interposition \
+    -funroll-loops -ftree-vectorize \
+    -fipa-pta \
+    line_checker.cpp -o line_checker.out
